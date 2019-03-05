22e36d249963e210f31e9afa2bcc50bac32df18e
skywolf123 <z4458622z>
2019-03-05 15:19:11 +0800

    second commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -8,6 +8,8 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -16,7 +18,10 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
+moment = Moment(app)
+babel = Babel(app)
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
@@ -34,9 +39,11 @@ if not app.debug:
         app.logger.addHandler(mail_handler)
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
     file_handler.setFormatter(
-        (logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s: %(lineno)d]')))
+        (logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s: %(lineno)d]')))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
@@ -44,3 +51,9 @@ if not app.debug:
     app.logger.info('Microblog startup')
 
 from app import routes, models, errors
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    # return 'zh'
--- app/email.py
+++ app/email.py
@@ -2,11 +2,12 @@ from flask_mail import Message
 from app import mail, app
 from flask import render_template
 from threading import Thread
+from flask_babel import _
 
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('[Microblog] Reset Your Password', sender=app.config['ADMINS'][0], recipients=[user.email],
+    send_email('[Microblog] '+_('Reset Your Password'), sender=app.config['ADMINS'][0], recipients=[user.email],
                text_body=render_template('email/reset_password.txt', user=user, token=token),
                html_body=render_template('email/reset_password.html', user=user, token=token))
 
