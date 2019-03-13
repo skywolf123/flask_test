3e4ae16eb600ad349e14439dd153f1442d2a96f8
skywolf123 <z4458622z>
2019-03-13 16:58:42 +0800

    second commit

--- app/models.py
+++ app/models.py
@@ -62,7 +62,11 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship(
+        'Post',
+        backref='author',
+        lazy='dynamic'
+    )
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow())
     followed = db.relationship(
@@ -71,6 +75,24 @@ class User(UserMixin, db.Model):
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic'
+    )
+    messages_sent = db.relationship(
+        'Message',
+        foreign_keys='Message.sender_id',
+        backref='author',
+        lazy='dynamic'
+    )
+    messages_received = db.relationship(
+        'Message',
+        foreign_keys='Message.recipient_id',
+        backref='recipient',
+        lazy='dynamic'
+    )
+    last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship(
+        'Notification',
+        backref='user',
         lazy='dynamic'
     )
 
@@ -106,8 +128,11 @@ class User(UserMixin, db.Model):
         return followed.union(own).order_by(Post.timestamp.desc())
 
     def get_reset_password_token(self, expires_in=600):
-        return jwt.encode({'reset_password': self.id, 'exp': time() + expires_in}, current_app.config['SECRET_KEY'],
-                          algorithm='HS256').decode('uft-8')
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            current_app.config['SECRET_KEY'],
+            algorithm='HS256'
+        ).decode('uft-8')
 
     @staticmethod
     def verify_reset_password_token(token):
@@ -116,6 +141,10 @@ class User(UserMixin, db.Model):
         except:
             return
         return User.query.get(id)
+
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(Message.timestamp > last_read_time).count()
 
 
 @login.user_loader
@@ -133,3 +162,13 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
